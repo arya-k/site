00a00e632a9cfcd13e6b309793407d73d42cbe07
Arya Kumar <arkumar@umich.edu>
2021-04-04 21:48:23 +0000

    Modified index page to new style

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     purge: ["./templates/**/*.html", "./theme/**/*.html"],
     theme: {
         extend: {
+            colors: {
+                'offwhite': '#fefbf4'
+            },
             fontFamily: {
+                mono: ['IBM Plex Mono', ...defaultTheme.fontFamily.mono],
                 sans: ['Raleway', ...defaultTheme.fontFamily.sans]
             }
         }
